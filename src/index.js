ff515c783ac9e56d11c97bb6bfb654569699b72c
Franny Xi Wu <franxiwu@gmail.com>
2025-03-23 09:19:30 -0400

    hack to force folders to be in the right directory because I can't figure out how to reconfigure it

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import App from './App';
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<App name='history trail'/>)
+root.render(<App name='China Dispossession Watch'/>)
